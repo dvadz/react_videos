ae637b1e36a1246d78a6abb1f627afdb46a9a2b2
Daniel Vadil <dvadz@yahoo.com>
2022-03-23 15:58:52 -0400

    Created a callback and pass down to the SearchBar form to etrieve the search term

--- src/components/App.js
+++ src/components/App.js
@@ -2,10 +2,14 @@ import React from "react";
 import SearchBar from "./SearchBar";
 
 class App extends React.Component {
+  onSubmit = (term) => {
+    console.log("Submitted", term);
+  };
+
   render() {
     return (
       <div className='app ui container'>
-        <SearchBar />
+        <SearchBar onSubmit={this.onSubmit} />
       </div>
     );
   }
