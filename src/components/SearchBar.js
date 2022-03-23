eb557ea0ba713056c85bfd96d5add2ec2872ea33
Daniel Vadil <dvadz@yahoo.com>
2022-03-23 15:08:23 -0400

    Put input in a form and added some styling and label

--- src/components/SearchBar.js
+++ src/components/SearchBar.js
@@ -3,8 +3,13 @@ import React from "react";
 class SearchBar extends React.Component {
   render() {
     return (
-      <div>
-        <input type='text' />
+      <div className='search-bar ui segment'>
+        <form className='ui form'>
+          <div>
+            <label for='search'>Search Youtube videos</label>
+            <input id='search' type='text' />
+          </div>
+        </form>
       </div>
     );
   }
