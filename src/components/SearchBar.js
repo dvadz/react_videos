b2553b28a5fb4fc79308d1ae18ce4b4869ca07f2
Daniel Vadil <dvadz@yahoo.com>
2022-03-23 15:37:08 -0400

    Wired an onChange handler for the input

--- src/components/SearchBar.js
+++ src/components/SearchBar.js
@@ -1,13 +1,19 @@
 import React from "react";
 
 class SearchBar extends React.Component {
+  state = { term: "" };
+
+  onInputChange = (e) => {
+    this.setState({ term: e.target.value });
+  };
+
   render() {
     return (
       <div className='search-bar ui segment'>
         <form className='ui form'>
           <div>
             <label for='search'>Search Youtube videos</label>
-            <input id='search' type='text' />
+            <input id='search' type='text' onChange={this.onInputChange} value={this.state.term} />
           </div>
         </form>
       </div>
