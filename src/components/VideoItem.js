4d654409e649bd2e6138ad971590c1ed4d1666e6
Daniel Vadil <dvadz@yahoo.com>
2022-03-24 11:46:56 -0400

    Added some styling to VideoItem with semantic-ui and css

--- src/components/VideoItem.js
+++ src/components/VideoItem.js
@@ -1,10 +1,11 @@
+import "../components/VideoItem.css";
 import React from "react";
 
 const VideoItem = ({ video }) => {
   return (
-    <div>
-      <img src={video.snippet.thumbnails.medium.url} />
-      <p>{video.snippet.title}</p>
+    <div className='video-item item'>
+      <img className='ui image' src={video.snippet.thumbnails.medium.url} />
+      <div className='description'>{video.snippet.title}</div>
     </div>
   );
 };
