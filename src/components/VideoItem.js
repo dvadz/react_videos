5c8bde4eac5b42b4fe81a414ffd7ef7941a1ffad
Daniel Vadil <dvadz@yahoo.com>
2022-03-24 11:58:30 -0400

    Assigned a key for each VideoItem

--- src/components/VideoItem.js
+++ src/components/VideoItem.js
@@ -3,7 +3,7 @@ import React from "react";
 
 const VideoItem = ({ video }) => {
   return (
-    <div className='video-item item'>
+    <div className='video-item item' key={video.id.videoId}>
       <img className='ui image' src={video.snippet.thumbnails.medium.url} />
       <div className='description'>{video.snippet.title}</div>
     </div>
