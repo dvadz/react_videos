3dd889b51b01b3b1caef8f356d8093db89e342c8
Daniel Vadil <dvadz@yahoo.com>
2022-03-23 22:49:41 -0400

    Configured axios for youtube search

---
+++ src/apis/youtube.js
@@ -0,0 +1,12 @@
+import axios from "axios";
+
+const KEY = "AIzaSyB6zkxJoh8OnMrjoxeETBOgBk-8D9N0EWw";
+
+export default axios.create({
+  baseURL: "https://www.googleapis.com/youtube/v3",
+  params: {
+    part: "snippet",
+    maxResults: 5,
+    key: KEY,
+  },
+});
