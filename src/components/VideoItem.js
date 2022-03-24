82b9ae612ee83aee8c426240bc56223191bec3f4
Daniel Vadil <dvadz@yahoo.com>
2022-03-24 01:25:11 -0400

    Show the video title and thumbnail

--- src/components/VideoItem.js
+++ src/components/VideoItem.js
@@ -1,7 +1,12 @@
 import React from "react";
 
-const VideoItem = (props) => {
-  return <div>VideoItem</div>;
+const VideoItem = ({ video }) => {
+  return (
+    <div>
+      <img src={video.snippet.thumbnails.medium.url} />
+      <p>{video.snippet.title}</p>
+    </div>
+  );
 };
 
 export default VideoItem;
