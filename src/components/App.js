2444b601cc38ac693986cc29a2cfe6dd729ee67d
Daniel Vadil <dvadz@yahoo.com>
2022-03-24 00:28:50 -0400

    Make a youtube query using the pre-configured axios

--- src/components/App.js
+++ src/components/App.js
@@ -1,9 +1,14 @@
 import React from "react";
 import SearchBar from "./SearchBar";
+import youtube from "../apis/youtube";
 
 class App extends React.Component {
-  onSubmit = (term) => {
-    console.log("Submitted", term);
+  onSubmit = async (term) => {
+    const response = await youtube.get("/search", {
+      params: {
+        q: term,
+      },
+    });
   };
 
   render() {
