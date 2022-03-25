e9582141763b1c330ee2b5eaf1a55a05f2175621
Daniel Vadil <dvadz@yahoo.com>
2022-03-25 02:26:57 -0400

    Added an iframe to play the video

--- src/components/VideoDetail.js
+++ src/components/VideoDetail.js
@@ -3,8 +3,13 @@ import React from "react";
 const VideoDetail = ({ video }) => {
   if (!video) return <div></div>;
 
+  const videoSrc = `https://www.youtube.com/embed/${video.id.videoId}`;
+
   return (
     <div>
+      <div className='ui embed'>
+        <iframe src={videoSrc} allowfullscreen></iframe>
+      </div>
       <div className='ui segment'>
         <h4 className='header'>{video.snippet.title}</h4>
         <p className='description'>{video.snippet.description}</p>
