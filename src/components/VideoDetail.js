3395736a9697366131c061cf6636da75fe4f5ff1
Daniel Vadil <dvadz@yahoo.com>
2022-03-25 02:04:17 -0400

    Showed video title and description inside VideoDetail

--- src/components/VideoDetail.js
+++ src/components/VideoDetail.js
@@ -3,7 +3,14 @@ import React from "react";
 const VideoDetail = ({ video }) => {
   if (!video) return <div></div>;
 
-  return <div>{video.snippet.title}</div>;
+  return (
+    <div>
+      <div className='ui segment'>
+        <h4 className='header'>{video.snippet.title}</h4>
+        <p className='description'>{video.snippet.description}</p>
+      </div>
+    </div>
+  );
 };
 
 export default VideoDetail;
